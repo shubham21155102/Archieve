b88ccab137f572d6b5875b0da1b40b9f7cfe261c
shubham <shubham.kumar.min21@itbhu.ac.in>
2023-12-23 15:38:29 +0530

    add web interface

--- DSA/LinkedList/js/DLL.js
+++ DSA/LinkedList/js/DLL.js
@@ -1,13 +1,15 @@
 class Node {
-    constructor(data, next = null) {
+    constructor(data, prev = null, next = null) {
         this.data = data;
+        this.prev = prev;
         this.next = next;
     }
-}
+};
 function insertAtHead(x, head) {
     if (head === null) return new Node(x);
     const temp = new Node(x);
     temp.next = head;
+    head.prev = temp;
     return temp;
 }
 function insertAtTail(x, head) {
@@ -16,30 +18,23 @@ function insertAtTail(x, head) {
     var curr = head;
     while (curr.next !== null) curr = curr.next;
     curr.next = temp;
+    temp.prev = curr;
     return head;
 }
 function printList(head) {
     var curr = head;
     while (curr !== null) {
         console.log(curr.data);
+        console.log(curr.prev);
         console.log(curr.next);
-
         curr = curr.next;
     }
 }
 var n1 = new Node(12);
 n1.next = new Node(14);
-
 n1.next.next = new Node(16);
-// n1.next.next.next=n1;
 n1 = insertAtHead(17, n1);
 n1 = insertAtHead(19, n1);
 n1 = insertAtTail(167, n1);
 console.log(n1);
 printList(n1);
-// Node {
-//     data: 12,
-//         next: Node { data: 14, next: Node { data: 16, next: null } }
-// }
-const app= document.getElementById('app');
-app.appendChild(n1);
