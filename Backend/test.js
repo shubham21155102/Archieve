d24a4d57b385251e6173443037742e322a92e299
shubham <shubham.kumar.min21@itbhu.ac.in>
2023-12-26 18:42:42 +0530

    add

--- Backend/test.js
+++ Backend/test.js
@@ -6,15 +6,29 @@
 // execSync('open https://www.bing.com/search?q=Bing%20Homepage%20quiz5&form=ML2BF1&OCID=ML2BF1&PUBL=RewardsDO&PROGRAMNAME=BingDailyOfferIN&CREA=ML2BF1');
 // execSync('open https://www.bing.com/search?q=Bing%20Homepage%20quiz6&form=ML2BF1&OCID=ML2BF1&PUBL=RewardsDO&PROGRAMNAME=BingDailyOfferIN&CREA=ML2BF1');
 // execSync('open https://www.bing.com/search?q=Bing%20Homepage%20quiz7&form=ML2BF1&OCID=ML2BF1&PUBL=RewardsDO&PROGRAMNAME=BingDailyOfferIN&CREA=ML2BF1');
-const EventEmitter=require("events");
-const event=new EventEmitter();
-event.on("sayMyName",()=>{
-    console.log("Your name is Shubham");
+// const EventEmitter=require("events");
+// const event=new EventEmitter();
+// event.on("sayMyName",()=>{
+//     console.log("Your name is Shubham");
+// });
+// event.on("sayMyName",()=>{
+//     console.log("Your name is Shubham Gupta");
+// });
+// event.on("sayMyName",()=>{
+//     console.log("Your name is Shubham Gupta and you are a web developer");
+// });
+// event.emit("sayMyName");
+const fs = require('fs');
+let data = '';
+let readStream = fs.createReadStream('Node_js.html');
+readStream.setEncoding('UTF8');
+readStream.on('data', function(chunk) {
+    data += chunk;
 });
-event.on("sayMyName",()=>{
-    console.log("Your name is Shubham Gupta");
+readStream.on('end', function() {
+    console.log(data);
 });
-event.on("sayMyName",()=>{
-    console.log("Your name is Shubham Gupta and you are a web developer");
+readStream.on('error', function(err) {
+    console.log(err.stack);
 });
-event.emit("sayMyName");
+console.log("Program Ended");
