ec1b3e4d11e5c0ee2722b54876ea27d6a93ec7a7
shubham <shubham.kumar.min21@itbhu.ac.in>
2024-01-13 01:32:20 +0530

    add quiz

---
+++ DSA/graph/_1_bfs.java
@@ -0,0 +1,17 @@
+package graph;
+import java.util.Scanner;
+import java.util.List;
+
+public class _1_bfs {
+    public static void main(String[] args) {
+        int n;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        
+    }
+    public static void bfs(List<Integer> adj[],int v,int s){
+      boolean visited[]=new boolean[v+1];
+      
+
+    }
+}
