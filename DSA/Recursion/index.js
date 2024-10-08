da24ab61b9b475abbf07fb3c2a6782092d4216f9
shubham <shubham.kumar.min21@itbhu.ac.in>
2024-10-09 01:23:22 +0530

    added sudoku solver visualisation

---
+++ DSA/Recursion/index.js
@@ -0,0 +1,65 @@
+function sudokuSolver(board) {
+    if (solve(board)) {
+        return board;
+    }
+    return "No solution";
+}
+
+function solve(board) {
+    let empty = findEmpty(board);
+    if (!empty) {
+        return true;
+    }
+    let [row, col] = empty;
+    for (let i = 1; i <= 9; i++) {
+        if (isValid(board, row, col, i)) {
+            board[row][col] = i;
+            if (solve(board)) {
+                return true;
+            }
+            board[row][col] = 0;
+        }
+    }
+    return false;
+}
+
+function findEmpty(board) {
+    for (let i = 0; i < board.length; i++) {
+        for (let j = 0; j < board[i].length; j++) {
+            if (board[i][j] === 0) {
+                return [i, j];
+            }
+        }
+    }
+    return null;
+}
+
+function isValid(board, row, col, num) {
+    for (let i = 0; i < 9; i++) {
+        if (board[row][i] === num || board[i][col] === num) {
+            return false;
+        }
+    }
+    let startRow = Math.floor(row / 3) * 3;
+    let startCol = Math.floor(col / 3) * 3;
+    for (let i = startRow; i < startRow + 3; i++) {
+        for (let j = startCol; j < startCol + 3; j++) {
+            if (board[i][j] === num) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+let board = [
+    [5, 3, 0, 0, 7, 0, 0, 0, 0],
+    [6, 0, 0, 1, 9, 5, 0, 0, 0],
+    [0, 9, 8, 0, 0, 0, 0, 6, 0],
+    [8, 0, 0, 0, 6, 0, 0, 0, 3],
+    [4, 0, 0, 8,3, 0, 0, 1, 0],
+    [7, 0, 0, 0, 2, 0, 0, 0, 6],
+    [0, 6, 0, 0, 0, 0, 2, 8, 0],
+    [0, 0, 0, 4, 1, 9, 0, 0, 5],
+    [0, 0, 0, 0, 8, 0, 0, 7, 9]
+];
